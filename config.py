994c0036a95bcd7324865c8658aa57f695bf45bc
Matthew Altenburg <matthew.altenburg@anu.edu.au>
2024-06-05 21:19:51 +1000

    added BabyJoeyConfig class

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-import yaml
+@dataclass
 
-class Config:
-    def __init__(self, config_path):
-        with open(config_path, 'r') as file:
-            self.config = yaml.safe_load(file)
-
-    def __getattr__(self, item):
-        return self.config.get(item, None)
+class BabyJoeyConfig:
+    vocab_size: int
+    n_embd: int
+    n_head: int
+    n_layer: int
+    max_position_embeddings: int = 512
