11c32ba4a7ffcd9968a7687fb57016123f9aac37
Matthew Altenburg <matthew.altenburg@anu.edu.au>
2024-10-04 08:09:19 +1000

    Refactor main.py: Move all TODOs to GitHub issues, streamline code
    All developmental notes previously marked as TODO in the code have been transferred to GitHub issues for better tracking and management. This commit further cleans up the main.py script by reorganizing imports, removing the TODO comments, and simplifying sections of the code to enhance readability and maintainability.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging 
-logging.getLogger("torchtnt").setLevel(logging.WARNING)  # suppress INFO logs from TorchTNT
-
+import logging
 import hydra
 import torch
 from torchtnt.framework.callbacks import TQDMProgressBar
@@ -13,31 +11,17 @@ from src.model import BabyJoeyModel
 from src.train import BabyJoeyUnit
 from src.utils import BabyJoeyUtil
 
+logging.getLogger("torchtnt").setLevel(logging.WARNING)  # suppress INFO logs from TorchTNT
 
-@hydra.main(version_base=None, config_name="baby_joey_config")  # TODO: Can users change the hard-coded config_name?
+@hydra.main(version_base=None, config_name="baby_joey_config")
 def main(config: BabyJoeyConfig):
-    
-    # TODO: The idea of changes in config is that:
-    # When people create datasets, they should only need to worry about the function to use - BabyJoeyDataset
-    #                                           and the configs corresponds to the function - DataConfig
-    # Another example, when creating dataloaders, users only need BabyJoeyDataLoader + DataLoaderConfig
-    # When creating BabyJoeyModel, it needs BabyJoeyModel + ModelConfig, not EmbeddingConfig or TransformerConfig
-    # In main.py, users should only call BabyJoeyFuncName with its corresponding NameConfig
-    
-    # Datasets <- DataConfig
-    # TODO: I want to change it to:
-    # train_dataset, val_dataset = BabyJoeyDataset(config.data)
-    # by designing a __new__() function inside BabyJoeyDataset
+    # Datasets
     train_dataset, val_dataset = BabyJoeyDataset(config.data).get_datasets()
-    # Dataloaders <- DataLoaderConfig
-    # TODO: Same as above. Can we do something about "state" to register datasets and dataloaders on it? Maybe not a good idea.
-    # train_dataloader, val_dataloader = BabyJoeyDataloader(config.dataloader)
+    
+    # Dataloaders
     train_dataloader, val_dataloader = BabyJoeyDataLoader(config.dataloader).get_dataloaders(train_dataset, val_dataset)
 
     # Model setup
-    # TODO: It's better to be:
-    # model = BabyJoeyModel(config.model)
-    # Users once configed device in config.py, they should not worry about device anymore.
     device = torch.device(config.training.device)
     model = BabyJoeyModel(config.model)
     
@@ -47,14 +31,11 @@ def main(config: BabyJoeyConfig):
     baby_joey_unit = BabyJoeyUnit(
         module=model,
         device=device,
-        # TODO: A bit ugly to use configs in this tedious way.
         lr=config.optimization.optimizer.learning_rate,
         weight_decay=config.optimization.optimizer.weight_decay,
         step_size=config.optimization.scheduler.step_size,
         gamma=config.optimization.scheduler.gamma
     )
-    # TODO: I manage to change the above code block into this single line.
-    # baby_joey_unit = BabyJoeyUnit(config.unit)(model)
 
     # Training loop
     fit(
@@ -63,12 +44,10 @@ def main(config: BabyJoeyConfig):
         eval_dataloader=val_dataloader,
         max_epochs=config.training.max_epochs,
         callbacks=[
-            TQDMProgressBar(), # FIXME: TQDMProgressBar's temporally fixed
+            TQDMProgressBar(),
             WandB(config.wandb)
         ]
     )
-    # TODO: I manage to change the above code block into this single line.
-    # fit(config.fit)(baby_joey_unit)  # TODO
 
 if __name__ == "__main__":
     main()
